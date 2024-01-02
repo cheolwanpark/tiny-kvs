375a891fc299c9f90d9204a38b36716b23ac9e16
CodingVillain <cheolwan.park552@gmail.com>
2024-01-02 15:01:32 +0900

    added clock policy frame management

--- db/src/pagemanager/inmemory.rs
+++ db/src/pagemanager/inmemory.rs
@@ -1,22 +1,120 @@
+use std::collections::HashMap;
 use super::*;
 
 pub struct InMemoryPageManager<T: PageManager> {
-    page_manager: Box<T>
+    page_manager: Box<T>,
+    frame_map: HashMap<PageId, usize>,
+    frames: Vec<PageFrame>,
+    clock_hand: usize,
+}
+
+#[derive(Clone)]
+struct PageFrame {
+    page_id: PageId,
+    data: Vec<u8>,
+    pin_count: u32,
+    is_dirty: bool,
+    ref_bit: bool,
 }
 
 impl<P: PageManager> InMemoryPageManager<P> {
-    pub fn new(page_manager: P) -> Self {
-        Self { page_manager: Box::new(page_manager) }
+    pub fn new(num_frames: usize, page_manager: P) -> Self {
+        Self { 
+            page_manager: Box::new(page_manager),
+            frame_map: HashMap::new(),
+            frames: vec![PageFrame {
+                page_id: 0,
+                data: vec![0; PAGE_SIZE],
+                pin_count: 0,
+                is_dirty: false,
+                ref_bit: false,
+            }; num_frames],
+            clock_hand: 0,
+        }
+    }
+
+    fn get_frame(&mut self, page_id: PageId) -> Option<&mut PageFrame> {
+        if let Some(frame_index) = self.frame_map.get(&page_id) {
+            Some(&mut self.frames[*frame_index])
+        } else {
+            None
+        }
+    }
+
+    fn find_victim(&mut self) -> usize {
+        let start_idx = self.clock_hand;
+        let mut second_loop = false;
+        loop {
+            let frame = &mut self.frames[self.clock_hand];
+            if frame.pin_count == 0 {
+                if frame.ref_bit {
+                    frame.ref_bit = false;
+                } else {
+                    return self.clock_hand;
+                }
+            }
+            self.clock_hand = (self.clock_hand + 1) % self.frames.len();
+            if self.clock_hand == start_idx {
+                if second_loop {
+                    panic!("All pages are pinned");
+                } else {
+                    second_loop = true;
+                }
+            }
+        }
+    }
+
+    fn evict_page(&mut self, idx: usize) {
+        let frame = &mut self.frames[idx];
+        if frame.pin_count > 0 {
+            panic!("Page is pinned");
+        }
+        if frame.is_dirty {
+            self.page_manager.write_page(frame.page_id, &frame.data).unwrap();
+            frame.is_dirty = false;
+        }
+        self.frame_map.remove(&frame.page_id);
     }
 }
 
 impl<P: PageManager> PageManager for InMemoryPageManager<P> {
     fn read_page<T: DeserializeOwned>(&mut self, id: PageId) -> std::io::Result<T> {
-        self.page_manager.read_page(id)
+        let frame = match self.get_frame(id) {
+            Some(frame) => frame,
+            None => {
+                let victim_idx = self.find_victim();
+                self.evict_page(victim_idx);
+                let frame = &mut self.frames[victim_idx];
+
+                let data: [u8; PAGE_SIZE] = self.page_manager.read_page(id)?;
+                frame.page_id = id;
+                frame.data.copy_from_slice(&data);
+                frame.pin_count = 0;
+                frame.is_dirty = false;
+                self.frame_map.insert(id, victim_idx);
+                frame
+            }
+        };
+        frame.pin_count += 1;
+        frame.ref_bit = true;
+        Ok(bincode::deserialize(&frame.data).unwrap())
     }
 
     fn write_page<T: Serialize>(&mut self, id: PageId, obj: &T) -> std::io::Result<()> {
-        self.page_manager.write_page(id, obj)
+        let frame = match self.get_frame(id) {
+            Some(frame) => frame,
+            None => {
+                let victim_idx = self.find_victim();
+                self.evict_page(victim_idx);
+                let frame = &mut self.frames[victim_idx];
+                frame.page_id = id;
+                frame.pin_count = 0;
+                frame
+            }
+        };
+        bincode::serialize_into(&mut frame.data, obj).unwrap();
+        frame.is_dirty = true;
+        Ok(())
     }
 
     fn alloc_page(&mut self) -> std::io::Result<PageId> {
@@ -31,7 +129,6 @@ impl<P: PageManager> PageManager for InMemoryPageManager<P> {
 mod test {
     use super::*;
     use std::{path::Path, fs};
-    use crate::pagemanager::diskbased::PAGE_SIZE;
     use rand::Rng;
 
     struct CleanupFileGuard<'a> {
@@ -52,6 +149,7 @@ mod test {
 
         let path = Path::new(filename);
         let mut disk_manager = InMemoryPageManager::new(
+            1024,
             DiskBasedPageManager::new(&path).unwrap()
         );
 
