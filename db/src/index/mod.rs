8eda1aca06553a31aad1608ffe35550fde81c205
CodingVillain <cheolwan.park552@gmail.com>
2024-03-04 15:01:49 +0900

    added Index and Index Accessor traits

---
+++ db/src/index/mod.rs
@@ -0,0 +1,13 @@
+
+
+pub trait Index {
+    fn insert(&mut self, elem: Box<dyn IndexAccessor>);
+    fn remove(&mut self, key: &str);
+    fn find(&self, key: &str) -> Option<Box<dyn IndexAccessor>>;
+    fn update(&mut self, elem: Box<dyn IndexAccessor>);
+}
+
+pub trait IndexAccessor {
+    fn key(&self) -> &str;
+    fn value(&self) -> &str;
+}
